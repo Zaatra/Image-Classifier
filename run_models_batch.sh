b638ce2873812790558ec2619866171944a29113
Mohammad Zaatra <imzaatra@gmail.com>
2024-06-01 01:37:56 +0300

    Created Results folder with adjusted output
    Adjusted Output of Shell files

--- run_models_batch.sh
+++ run_models_batch.sh
@@ -1,3 +1,3 @@
-python check_images.py --dir pet_images/ --arch resnet  --dogfile dognames.txt > resnet_pet-images.txt
-python check_images.py --dir pet_images/ --arch alexnet --dogfile dognames.txt > alexnet_pet-images.txt
-python check_images.py --dir pet_images/ --arch vgg  --dogfile dognames.txt > vgg_pet-images.txt
+python check_images.py --dir pet_images/ --arch resnet  --dogfile dognames.txt > Results/resnet_pet-images.txt
+python check_images.py --dir pet_images/ --arch alexnet --dogfile dognames.txt > Results/alexnet_pet-images.txt
+python check_images.py --dir pet_images/ --arch vgg  --dogfile dognames.txt > Results/vgg_pet-images.txt
--- run_models_batch_uploaded.sh
+++ run_models_batch_uploaded.sh
@@ -1,3 +1,3 @@
-python check_images.py --dir uploaded_images/ --arch resnet  --dogfile dognames.txt > resnet_uploaded-images.txt
-python check_images.py --dir uploaded_images/ --arch alexnet --dogfile dognames.txt > alexnet_uploaded-images.txt
-python check_images.py --dir uploaded_images/ --arch vgg  --dogfile dognames.txt > vgg_uploaded-images.txt
+python check_images.py --dir uploaded_images/ --arch resnet  --dogfile dognames.txt > Results/resnet_uploaded-images.txt
+python check_images.py --dir uploaded_images/ --arch alexnet --dogfile dognames.txt > Results/alexnet_uploaded-images.txt
+python check_images.py --dir uploaded_images/ --arch vgg  --dogfile dognames.txt > Results/vgg_uploaded-images.txt
