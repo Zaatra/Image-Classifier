e0dcb449b1437d68e25131d25195de59e2fbac7e
Mohammad Zaatra <imzaatra@gmail.com>
2024-06-01 02:52:36 +0300

    Added choices parameter for Arguments

--- get_input_args.py
+++ get_input_args.py
@@ -1,11 +1,15 @@
 import argparse
+
 def get_input_args():
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--dir', type=str, default='pet_images/', 
-    help='Path to the folder of pet images') 
-    parser.add_argument('--arch', type=str, default='vgg', 
-    help='CNN model architecture to use: ResNet, AlexNet, or VGG')
+                       choices=['pet_images/', 'uploaded_images/'],                         
+                       help='Path to the folder of pet images: pet_images , uploaded_images') 
+    parser.add_argument('--arch', type=str, default='vgg',
+                       choices=['vgg', 'alexnet', 'resnet'], 
+                       help='CNN model architecture to use: ResNet, AlexNet, or VGG')
     parser.add_argument('--dogfile', type=str, default='dognames.txt',
-    help='File that contains the list of valid dognames')
+                       help='File that contains the list of valid dognames')
     in_arg = parser.parse_args()
     return in_arg
