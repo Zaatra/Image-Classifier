fbd2a341bb733ce9e020d0c4b170a855c9b3def1
Mohammad Zaatra <imzaatra@gmail.com>
2024-06-01 04:32:56 +0300

    Simplify reading the dog file.
    Use set instead of dict for dognames_dic for better performance.

--- adjust_results_isadog.py
+++ adjust_results_isadog.py
@@ -1,36 +1,8 @@
 def adjust_results_isadog(results_dic, dogfile):
-    """
-    Adjusts the results dictionary to determine if an image is a dog
-    (and to identify the breed if applicable).
-    
-    Parameters:
-      results_dic - Dictionary with 'key' as image filename and 'value' as a 
-        List. The list contains for one image, the pet image label (string),
-        the classifier label (string), and match (int).
-      dogfile - A string representing the filename in which the dog names are 
-        stored.
-    
-    Returns:
-      None - modifies the results_dic dictionary in place
-    """
-    dognames_dic = dict()
     with open(dogfile, "r") as infile:
-        line = infile.readline()
-        while line != "":
-            line = line.rstrip()
-            if line not in dognames_dic:
-                dognames_dic[line] = 1
-            else:
-                print("Duplicate Dogname", line)
-            line = infile.readline()
+        dognames = set(line.strip() for line in infile)
 
-    # Use .values() to iterate over the dictionary values directly
     for value in results_dic.values():
-        # Check if the pet image label is a dog
-        is_dog = value[0] in dognames_dic
-
-        # Check if the classifier label is a dog
-        is_classifier_dog = value[1] in dognames_dic
-
-        # Extend the value list with dog and breed match indicators
-        value.extend((int(is_dog), int(is_classifier_dog))) 
+        is_dog = int(value[0] in dognames)
+        is_classifier_dog = int(value[1] in dognames)
+        value.extend((is_dog, is_classifier_dog))
